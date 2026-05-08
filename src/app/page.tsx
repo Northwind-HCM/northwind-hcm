2384cd309f7258dcac904e7ae86711f361801ee4
Roland Schrader <roland.schrader@northwind-hr.de>
2026-05-08 20:22:20 +0200

    Fix locale routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-import Navbar from "@/components/landing/Navbar";
-import HeroSection from "@/components/landing/HeroSection";
-import FeatureGrid from "@/components/landing/FeatureGrid";
-import PricingSection from "@/components/landing/PricingSection";
-import SecuritySection from "@/components/landing/SecuritySection";
-import Footer from "@/components/landing/Footer";
+import { redirect } from "next/navigation";
 
-
-export default function HomePage() {
-  return (
-    <main className="min-h-screen bg-white">
-      <Navbar />
-      <HeroSection />
-      <FeatureGrid />
-      <FeatureGrid />
-<PricingSection />
-<SecuritySection />
-<Footer />
-    </main>
-  );
+export default function RootPage() {
+  redirect("/de");
 }

0263bb008d1c64cb768af82706854f5819bff4e1
Roland Schrader <roland.schrader@northwind-hr.de>
2026-05-13 20:13:46 +0200

    fix: force dynamic sitemap rendering

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,6 @@
 import type { MetadataRoute } from "next";
+export const dynamic = 'force-dynamic';
+
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://northwind-hr.eu";
